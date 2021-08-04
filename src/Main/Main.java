c761655aeb905fd1cc59e61c538eb3316c8273a0
Juan Felipe Marin Florez <jumarin@javeriana.edu.co>
2021-08-04 16:18:35 -0500

    added the dijkstra algorithm for the best path between two nodes

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,7 +4,6 @@ import Graph.Graph;
 import Persistence.Persistence;
 
 import java.io.*;
-import java.lang.Math;
 
 
 public class Main {
@@ -18,30 +17,44 @@ public class Main {
 
 
 
-       for (int i=0; i<10; i++){
-           migrafito.addNodo("n"+String.valueOf(i));
+       for (int i=1; i<10; i++){
+           migrafito.addNode("n"+String.valueOf(i));
 
        }
 
-        //migrafito.verNodos();
-       // migrafito.verAdyacencia();
+       /*migrafito.addVertice("n1",2,"n2");
+       migrafito.addVertice("n1",3,"n3");
+       migrafito.addVertice("n2",5,"n4");
+       migrafito.addVertice("n2",2,"n5");
+       migrafito.addVertice("n3",5,"n5");
+       migrafito.addVertice("n4",1,"n5");
+       migrafito.addVertice("n4",2,"n6");
+       migrafito.addVertice("n5",4,"n6");*/
+
 
         for (int i=0; i<15; i++){
-            int numeroRandome = (int)(Math.random()*(10)+0);
-            int numeroRandome2 = (int)(Math.random()*(10)+0);
-            migrafito.addVertice("n"+String.valueOf(numeroRandome),
+            int numeroRandome = (int)(Math.random()*(10)+1);
+            int numeroRandome2 = (int)(Math.random()*(10)+1);
+            if(numeroRandome!=numeroRandome2)
+            migrafito.addEdge("n"+String.valueOf(numeroRandome),
                     i,"n"+String.valueOf(numeroRandome2));
 
 
         }
 
+
+
+        System.out.println(migrafito.getNodes().toArray()[0]);
+         migrafito.seeAdjacency();
+
+        migrafito.dijkstra("n1","n9");
         //migrafito.verNodos();
        // migrafito.verAdyacencia();
 
         Persistence pers = new Persistence();
         pers.setGrafo(migrafito);
         pers.serializar("graph.txt");
-        pers.getGrafo().verAdyacencia();
+        //pers.getGrafo().seeAdjacency();
 
 
 
